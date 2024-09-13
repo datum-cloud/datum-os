5863bb3f0f9081cc9857b5853390a53e217f2c68
Aus Lacroix <aus@littlebearlabs.io>
2024-09-13 13:53:33 -0700

    Generate migration for new contact list schema.

---
+++ packages/datum-service/db/migrations/20240913205205_feature-contact-lists.sql
@@ -0,0 +1,24 @@
+-- Create "contact_list_history" table
+CREATE TABLE "contact_list_history" ("id" character varying NOT NULL, "history_time" timestamptz NOT NULL, "ref" character varying NULL, "operation" character varying NOT NULL, "created_at" timestamptz NULL, "updated_at" timestamptz NULL, "created_by" character varying NULL, "updated_by" character varying NULL, "deleted_at" timestamptz NULL, "deleted_by" character varying NULL, "mapping_id" character varying NOT NULL, "tags" jsonb NULL, "owner_id" character varying NULL, "name" character varying NOT NULL, "visibility" character varying NOT NULL DEFAULT 'PRIVATE', "display_name" character varying NOT NULL DEFAULT '', "description" character varying NULL, PRIMARY KEY ("id"));
+-- Create index "contactlisthistory_history_time" to table: "contact_list_history"
+CREATE INDEX "contactlisthistory_history_time" ON "contact_list_history" ("history_time");
+-- Create "contact_list_membership_history" table
+CREATE TABLE "contact_list_membership_history" ("id" character varying NOT NULL, "history_time" timestamptz NOT NULL, "ref" character varying NULL, "operation" character varying NOT NULL, "created_at" timestamptz NULL, "updated_at" timestamptz NULL, "created_by" character varying NULL, "updated_by" character varying NULL, "mapping_id" character varying NOT NULL, "deleted_at" timestamptz NULL, "deleted_by" character varying NULL, "contact_list_id" character varying NOT NULL, "contact_id" character varying NOT NULL, PRIMARY KEY ("id"));
+-- Create index "contactlistmembershiphistory_history_time" to table: "contact_list_membership_history"
+CREATE INDEX "contactlistmembershiphistory_history_time" ON "contact_list_membership_history" ("history_time");
+-- Create "contact_lists" table
+CREATE TABLE "contact_lists" ("id" character varying NOT NULL, "created_at" timestamptz NULL, "updated_at" timestamptz NULL, "created_by" character varying NULL, "updated_by" character varying NULL, "deleted_at" timestamptz NULL, "deleted_by" character varying NULL, "mapping_id" character varying NOT NULL, "tags" jsonb NULL, "name" character varying NOT NULL, "visibility" character varying NOT NULL DEFAULT 'PRIVATE', "display_name" character varying NOT NULL DEFAULT '', "description" character varying NULL, "owner_id" character varying NULL, PRIMARY KEY ("id"), CONSTRAINT "contact_lists_organizations_contact_lists" FOREIGN KEY ("owner_id") REFERENCES "organizations" ("id") ON UPDATE NO ACTION ON DELETE SET NULL);
+-- Create index "contact_lists_mapping_id_key" to table: "contact_lists"
+CREATE UNIQUE INDEX "contact_lists_mapping_id_key" ON "contact_lists" ("mapping_id");
+-- Create index "contactlist_name_owner_id" to table: "contact_lists"
+CREATE UNIQUE INDEX "contactlist_name_owner_id" ON "contact_lists" ("name", "owner_id") WHERE (deleted_at IS NULL);
+-- Create "contact_list_memberships" table
+CREATE TABLE "contact_list_memberships" ("id" character varying NOT NULL, "created_at" timestamptz NULL, "updated_at" timestamptz NULL, "created_by" character varying NULL, "updated_by" character varying NULL, "mapping_id" character varying NOT NULL, "deleted_at" timestamptz NULL, "deleted_by" character varying NULL, "contact_list_id" character varying NOT NULL, "contact_id" character varying NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "contact_list_memberships_contact_lists_contact_list" FOREIGN KEY ("contact_list_id") REFERENCES "contact_lists" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT "contact_list_memberships_contacts_contact" FOREIGN KEY ("contact_id") REFERENCES "contacts" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION);
+-- Create index "contact_list_memberships_mapping_id_key" to table: "contact_list_memberships"
+CREATE UNIQUE INDEX "contact_list_memberships_mapping_id_key" ON "contact_list_memberships" ("mapping_id");
+-- Create index "contactlistmembership_contact_id_contact_list_id" to table: "contact_list_memberships"
+CREATE UNIQUE INDEX "contactlistmembership_contact_id_contact_list_id" ON "contact_list_memberships" ("contact_id", "contact_list_id") WHERE (deleted_at IS NULL);
+-- Modify "events" table
+ALTER TABLE "events" ADD COLUMN "contact_list_events" character varying NULL, ADD COLUMN "contact_list_membership_events" character varying NULL, ADD CONSTRAINT "events_contact_list_memberships_events" FOREIGN KEY ("contact_list_membership_events") REFERENCES "contact_list_memberships" ("id") ON UPDATE NO ACTION ON DELETE SET NULL, ADD CONSTRAINT "events_contact_lists_events" FOREIGN KEY ("contact_list_events") REFERENCES "contact_lists" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
+-- Modify "integrations" table
+ALTER TABLE "integrations" ADD COLUMN "contact_list_integrations" character varying NULL, ADD CONSTRAINT "integrations_contact_lists_integrations" FOREIGN KEY ("contact_list_integrations") REFERENCES "contact_lists" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
