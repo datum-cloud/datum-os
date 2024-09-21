ea1257279343d206339cca316fcadf454d8d1af3
Angus <37071175+angusbezzina@users.noreply.github.com>
2024-09-21 10:03:53 -0500

    styles: login page

--- packages/datum-web/apps/operator/src/app/(auth)/login/page.styles.tsx
+++ packages/datum-web/apps/operator/src/app/(auth)/login/page.styles.tsx
@@ -5,6 +5,7 @@ const pageStyles = tv({
     logo: 'flex justify-center mb-10',
     bg: 'absolute h-full w-2/3 top-0 left-0 opacity-0 transition-opacity duration-700',
     bgImage: 'object-cover object-center h-full z-10',
+    container: 'flex flex-col h-full max-h-full w-full py-12 overflow-auto',
     content:
       'relative z-20 bg-white shadow-auth rounded-lg flex flex-col justify-center mx-auto my-auto py-16 px-12 w-full max-w-lg',
   },
