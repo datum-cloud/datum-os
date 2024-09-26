7edbebe251ec405f002d0ed3458fd25a81629f66
Angus <37071175+angusbezzina@users.noreply.github.com>
2024-09-26 10:37:38 -0500

    style: loading page background colour

--- packages/datum-web/apps/operator/src/app/page.tsx
+++ packages/datum-web/apps/operator/src/app/page.tsx
@@ -28,12 +28,14 @@ const Landing = () => {
   }, [fetching, session, orgs, router])
 
   return (
-    <main className="flex items-center justify-center h-screen relative bg-winter-sky-900 dark:bg-peat-900">
+    <main className="flex items-center justify-center h-screen relative bg-blackberry-800 dark:bg-peat-900">
       <div className="w-full relative z-3 px-4">
         <div className="mx-auto animate-pulse w-96">
           <Logo theme="dark" />
         </div>
-        <h1 className="text-2xl text-center mt-4">loading your platform...</h1>
+        <h1 className="text-2xl text-center text-white mt-4">
+          loading your platform...
+        </h1>
       </div>
     </main>
   )
