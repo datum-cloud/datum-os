6bdfaa4cebbc0b98f1c6c5382dd41f79099771dd
Aus Lacroix <aus@littlebearlabs.io>
2024-09-26 02:03:37 -0700

    Feature Flag Provider with Hook. Server Side Feature Flag Client. Docker Support. Env updates.

--- packages/datum-web/apps/operator/src/app/providers.tsx
+++ packages/datum-web/apps/operator/src/app/providers.tsx
@@ -5,7 +5,7 @@ import { createClient } from '@/lib/urql'
 import { useSession } from 'next-auth/react'
 import { ThemeProvider } from '@/providers/theme'
 import { ClientProvider } from '@/query/client-provider'
-
+import { FeatureFlagsProvider } from '@/lib/feature-flags/feature-flags-provider'
 interface ProvidersProps {
   children: any
 }
@@ -13,18 +13,24 @@ interface ProvidersProps {
 const Providers = ({ children }: ProvidersProps) => {
   const { data: session } = useSession()
   const client = createClient(session)
+  const context = {
+    targetingKey: session?.user.userId,
+    email: session?.user.email
+  };
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <ClientProvider>
-        <GraphqlProvider value={client}>{children}</GraphqlProvider>
-      </ClientProvider>
-    </ThemeProvider>
+    <FeatureFlagsProvider context={context}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ClientProvider>
+          <GraphqlProvider value={client}>{children}</GraphqlProvider>
+        </ClientProvider>
+      </ThemeProvider>
+    </FeatureFlagsProvider>
   )
 }
 
---
+++ packages/datum-web/apps/operator/src/lib/feature-flags/feature-flags.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import { OpenFeature } from '@openfeature/server-sdk'
+import { GoFeatureFlagProvider } from '@openfeature/go-feature-flag-provider'
+import { auth } from '@/lib/auth/auth'
+
+const goFeatureFlagProvider: GoFeatureFlagProvider = new GoFeatureFlagProvider({
+  endpoint: process.env.FEATURE_FLAG_PROVIDER_URL!
+})
+
+OpenFeature.setProvider(goFeatureFlagProvider)
+
+const featureFlagClient = OpenFeature.getClient('operator')
+
+export async function getFeatureFlag(
+  flagKey: string,
+  defaultValue: any
+) {
+  const session = await auth()
+  const context = {
+    targetingKey: session?.user.userId,
+    email: session?.user.email
+  }
+  return featureFlagClient.getBooleanValue(flagKey, defaultValue, context)
+}
