fdb879f474a1fcd617d3cfca4a5983595783a5b3
Aus Lacroix <aus@littlebearlabs.io>
2024-09-26 20:32:38 -0700

    Revert test change.

--- packages/datum-web/apps/operator/src/app/(protected)/profile/page.tsx
+++ packages/datum-web/apps/operator/src/app/(protected)/profile/page.tsx
@@ -3,16 +3,9 @@ import { AvatarUpload } from '@/components/shared/avatar-upload/avatar-upload'
 import { pageStyles } from './page.styles'
 import { ProfileNameForm } from '@/components/pages/protected/profile/user-settings/profile-name-form'
 import PageTitle from '@/components/page-title'
-import { getFeatureFlagBool } from '@/providers/feature-flags-server'
 
 export const metadata: Metadata = {
   title: 'Workspace settings',
-}
-
-const featureFlagPanel = async () => {
-  const localTest = await getFeatureFlagBool('local-test', false)
-  console.log('hello from the client')
-  return <div>{localTest ? 'the flag was true' : 'the flag was false'}</div>
 }
 
 const Page: React.FC = () => {
@@ -23,9 +16,6 @@ const Page: React.FC = () => {
       <div className={wrapper()}>
         <ProfileNameForm />
         <AvatarUpload />
-        <div className="flex flex-col gap-4">
-          {featureFlagPanel()}
-        </div>
       </div>
     </>
   )
