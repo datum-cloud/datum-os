d9e380ee22dd50e27f22d9cb80bcb1f063258d14
Ghislain 'Aus' Lacroix <1350717+nullaus@users.noreply.github.com>
2024-09-27 09:06:26 -0700

    Support for API keys when fetching feature flags (#57)
    * For testing feature flags in production
    
    * Renamed provider file
    
    * Revert test change.
    
    * Test with api keys
    
    * Remove test code
    
    * Ensure context exists, throw error if it does not

--- packages/datum-web/apps/operator/src/providers/feature-flags-server.ts
+++ packages/datum-web/apps/operator/src/providers/feature-flags-server.ts
@@ -5,7 +5,8 @@ import { GoFeatureFlagProvider } from '@openfeature/go-feature-flag-provider'
 import { auth } from '@/lib/auth/auth'
 
 const goFeatureFlagProvider: GoFeatureFlagProvider = new GoFeatureFlagProvider({
-  endpoint: process.env.FEATURE_FLAG_PROVIDER_URL!,
+  endpoint: process.env.NEXT_PUBLIC_FEATURE_FLAG_PROVIDER_URL!,
+  apiKey: process.env.NEXT_PUBLIC_FEATURE_FLAG_PROVIDER_API_KEY!,
 })
 
 OpenFeature.setProvider(goFeatureFlagProvider)
@@ -31,17 +32,49 @@ const context = async () => {
 // const localTest = getFeatureFlagBool('local-test', false)
 //
 export async function getFeatureFlagBool(flagKey: string, defaultValue: any) {
-  return featureFlagClient.getBooleanValue(flagKey, defaultValue, await context())
+  const ctx = await context()
+  if (!ctx) {
+    throw new Error('Failed to get context for feature flag')
+  }
+  return featureFlagClient.getBooleanValue(
+    flagKey,
+    defaultValue,
+    ctx,
+  )
 }
 
 export async function getFeatureFlagString(flagKey: string, defaultValue: any) {
-  return featureFlagClient.getStringValue(flagKey, defaultValue, await context())
+  const ctx = await context()
+  if (!ctx) {
+    throw new Error('Failed to get context for feature flag')
+  }
+  return featureFlagClient.getStringValue(
+    flagKey,
+    defaultValue,
+    ctx,
+  )
 }
 
 export async function getFeatureFlagNumber(flagKey: string, defaultValue: any) {
-  return featureFlagClient.getNumberValue(flagKey, defaultValue, await context())
+  const ctx = await context()
+  if (!ctx) {
+    throw new Error('Failed to get context for feature flag')
+  }
+  return featureFlagClient.getNumberValue(
+    flagKey,
+    defaultValue,
+    ctx,
+  )
 }
 
 export async function getFeatureFlagObject(flagKey: string, defaultValue: any) {
-  return featureFlagClient.getObjectValue(flagKey, defaultValue, await context())
+  const ctx = await context()
+  if (!ctx) {
+    throw new Error('Failed to get context for feature flag')
+  }
+  return featureFlagClient.getObjectValue(
+    flagKey,
+    defaultValue,
+    ctx,
+  )
 }
